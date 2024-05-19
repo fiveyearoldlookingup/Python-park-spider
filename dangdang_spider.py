a4471771bbf6c3b88a7aa494a90850139ea560fa
fiveyearoldlookingup <132869584+fiveyearoldlookingup@users.noreply.github.com>
2024-05-19 21:18:07 +0800

    5.19

---
+++ dangdang_spider.py
@@ -0,0 +1,60 @@
+import requests
+import re
+import json
+
+
+
+def request_dandan(url):
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+
+        response = requests.get(url, headers=headers)
+        #response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+    except requests.RequestException as e:
+        print(e)
+        return None
+
+#用来封装
+def parse_result(html):
+    pattern = re.compile(
+        '<li.*?list_num.*?(\d+)\.</div>.*?< img src="(.*?)".*?class="name".*?title="(.*?)">.*?class="star">.*?class="tuijian">(.*?)</span>.*?class="publisher_info">.*?target="_blank">(.*?)</a >.*?class="biaosheng">.*?<span>(.*?)</span></div>.*?<p><span class="price_n">(.*?)</span>.*?</li>', re.S)
+    items = re.findall(pattern, html)
+
+    for item in items:
+        yield {
+            'range': item[0],
+            'image': item[1],
+            'title': item[2],
+            'recommend': item[3],
+            'author': item[4],
+            'times': item[5],
+            'price': item[6]
+        }
+    for item in items:
+       print(item)
+
+
+def write_item_to_file(item):
+    print('开始写入数据 ====> ' + str(item))
+    with open('book.txt', 'a', encoding='UTF-8') as f:
+        f.write(json.dumps(item, ensure_ascii=False) + '\n')
+        f.close()
+
+
+def main(page):
+    url = 'http://bang.dangdang.com/books/fivestars/01.00.00.00.00.00-recent30-0-0-1-' + str(page)
+    html = request_dandan(url)
+    items = parse_result(html)  # 解析过滤我们想要的信息
+    for item in items:
+        write_item_to_file(item)
+
+
+if __name__ == "__main__":
+    for i in range(1, 2):
+        print(f"Processing page {i}")
+
+        main(i)
+
+#python E:\Github\Python-Park\dangdang_spider.py
